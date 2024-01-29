c57d4e09f879d82767d7b85a89ca6fe04fe28b8c
omaralkhuly18 <omaralkhuly@gmail.com>
2024-01-29 20:55:55 +0200

    api_learn_one_A-B-C-D-E-FF-C

--- Prayer_Timings.js
+++ Prayer_Timings.js
@@ -1,82 +1,74 @@
-let cities = [
-    " إختار إسم المدينه/المحافظه/الولايه ",
-    " الدقهلية ",
-    " البحر الأحمر ",
-    " البحيرة ",
-    " الفيوم ",
-    "الغربية",
-    " الإسكندريه ",
-    " الإسماعيلية ",
-    " الجيزة ",
-    " المنُوفيّة ",
-    " المنيا ",
-    " القاهره ",
-    " القليوبية ",
-    " الأقصر ",
-    " الوادي الجديد ",
-    " السويس ",
-    " الشرقية ",
-    " أسوان ",
-    " أسيوط ",
-    " بني سويف ",
-    " بورسعيد ",
-    " دمياط ",
-    " جنوب سيناء ",
-    "  كفر الشيخ ",
-    " مطروح ",
-    " قنا ",
-    " شمال سيناء ",
-    " سوهاج "
-];
-document.getElementById("cities_selected");
+let cities = {
+    "الدقهلية": "Dakahlia",
+    "البحر الأحمر": "BA",
+    "البحيرة": "Beheira",
+    "الفيوم": "Faiyum",
+    "الغربية": "Gharbia",
+    "الإسكندرية": "Alexandria",
+    "الإسماعيلية": "Ismailia",
+    "الجيزة": "Giza",
+    "المنُوفيّة": "Monufia",
+    "المنيا": "Minya",
+    "القاهرة": "Cairo",
+    "القليوبية": "Qalyubia",
+    "الأقصر": "Luxor",
+    "الوادي الجديد": "WAD",
+    "السويس": "Suez",
+    "الشرقية": "Al Sharqia",
+    "أسوان": "Aswan",
+    "أسيوط": "Asyut",
+    "بني سويف": "BNS",
+    "بورسعيد": "PTS"
+};
 
-for (let city of cities) {
-    const contact = `
-    <option>${city}</optiion>
-    `
-    document.getElementById("cities_selected").innerHTML += contact;
+for (let city in cities) {
+    if (cities.hasOwnProperty(city)) {
+        const contact = `
+        <option>${city}</option>`;
+        document.getElementById("cities_selected").innerHTML += contact;
+    }
 }
 
-document.getElementById("cities_selected").addEventListener('change', () => {
-    
-    if(this.value == "الدقهلية"){
-        getPrayersTimngsOfCity("Dakahlia")
-    }else if(this.value == "البحر الأحمر"){
-        getPrayersTimngsOfCity("BA")
+document.getElementById("cities_selected").addEventListener('change', function() {
+    const selectedCity = this.value.trim();
+    document.getElementById("name_city").innerHTML = selectedCity;
+    if (selectedCity in cities) {
+        getPrayersTimingsOfCity(cities[selectedCity]);
     }
-    console.log(this.value)
 });
 
-
-function getPrayersTimngsOfCity(cityName) {
+function getPrayersTimingsOfCity(cityName) {
     let params = {
         country: "EG",
-        city: cityName //"C"
+        city: cityName
     };
     axios.get('http://api.aladhan.com/v1/timingsByCity', {
         params: params
     })
-        .then(function (response) {
-            const timings = response.data.data.timings;
+    .then(function (response) {
+        const timings = response.data.data.timings;
 
-            fillTimeForPrayer("Fajr_time", timings.Fajr);
-            fillTimeForPrayer("Sunrise_time", timings.Sunrise);
-            fillTimeForPrayer("Dhuhr_time", timings.Dhuhr);
-            fillTimeForPrayer("Asr_time", timings.Asr);
-            fillTimeForPrayer("Maghrib_time", timings.Maghrib);
-            fillTimeForPrayer("Isha_time", timings.Isha);
+        fillTimeForPrayer("Fajr_time", timings.Fajr);
+        fillTimeForPrayer("Sunrise_time", timings.Sunrise);
+        fillTimeForPrayer("Dhuhr_time", timings.Dhuhr);
+        fillTimeForPrayer("Asr_time", timings.Asr);
+        fillTimeForPrayer("Maghrib_time", timings.Maghrib);
+        fillTimeForPrayer("Isha_time", timings.Isha);
 
-            const readableDate = response.data.data.date.readable;
-            const weekDay = response.data.data.date.hijri.weekday.ar;
-            const date = weekDay + " " + readableDate;
-            document.getElementById("newDate").innerHTML = date;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        const readableDate = response.data.data.date.readable;
+        const weekDay = response.data.data.date.hijri.weekday.ar;
+        const date = weekDay + " " + readableDate;
+        document.getElementById("newDate").innerHTML = date;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 }
-getPrayersTimngsOfCity("Dakahlia");
+
+// Initial call with a default city
+getPrayersTimingsOfCity(" القاهرة ");
 
 function fillTimeForPrayer(id, time) {
     document.getElementById(id).innerHTML = time;
 }
+
