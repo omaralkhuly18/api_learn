5e723321c501171c8344ebb56779921349d31be4
omaralkhuly18 <omaralkhuly18@gmail.com>
2024-02-12 14:21:47 +0200

    api_learn_one_A-B

--- script_apiP_tow.js
+++ script_apiP_tow.js
@@ -1,17 +1,18 @@
-
-
-
-
+// create function get of all posts start
 function GetPosts(userId) {
+    // Create an object from the basic class in the form of arrays in the API
     let newRequest = new XMLHttpRequest();
 
     newRequest.open("GET", "https://jsonplaceholder.typicode.com/posts?userId="+userId);
+    // Convert output type from objects to string
     newRequest.responseType = "json"
     newRequest.send();
 
     newRequest.onload = function () {
+        // Check the status number of the link in the postman
         if (newRequest.status >= 200 && newRequest.status < 300) {
             let posts = newRequest.response;
+            // Empty items of their original content
             document.getElementById("posts").innerHTML = "";
             for (post of posts) {
                 
@@ -29,17 +30,23 @@ function GetPosts(userId) {
         }
     }
 }
+// create function get of all posts end
 
+// create function get of all users start
 function GetUsers() {
+    // Create an object from the basic class in the form of arrays in the API
     let newRequest = new XMLHttpRequest();
 
     newRequest.open("GET", "https://jsonplaceholder.typicode.com/users");
+    // Convert output type from objects to string
     newRequest.responseType = "json"
     newRequest.send();
 
     newRequest.onload = function () {
+        // Check the status number of the link in the postman
         if (newRequest.status >= 200 && newRequest.status < 300) {
             let users = newRequest.response;
+            // Empty items of their original content
             document.getElementById("users").innerHTML = "";
             for (user of users) {
                 let content = `
@@ -54,7 +61,9 @@ function GetUsers() {
         }
     }
 }
+// create function get of all users end
 
+// create function click on only user to get One user's post start
 function userClicked(id , el){
     GetPosts(id);
 
@@ -65,6 +74,9 @@ function userClicked(id , el){
 
     el.classList.add("selected");
 }
+// create function click on only user to get One user's post end
 
+// Function keys start
 GetUsers();
 GetPosts(1);
+// Function keys end
